bf5835c60110d41a2768485b5d8c74769554e6ab
Leeple <kple1@naver.com>
2024-02-20 17:03:11 +0900

    LeepleLib

---
+++ src/main/java/io/leeple/network/Main.java
@@ -0,0 +1,41 @@
+package io.leeple.network;
+
+import io.leeple.network.SQL.SQLSetting;
+
+import java.sql.SQLException;
+
+public class Main implements SQLSetting {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void dataInput() throws SQLException {
+
+    }
+
+    @Override
+    public void databaseExists() throws SQLException {
+
+    }
+
+    @Override
+    public void dropDataBase() throws SQLException {
+
+    }
+
+    @Override
+    public void createDataBase() throws SQLException {
+
+    }
+
+    @Override
+    public void auth() throws SQLException {
+
+    }
+
+    @Override
+    public void createTable() throws SQLException {
+
+    }
+}
